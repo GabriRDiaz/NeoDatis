4a550ee5981b16a8613c2a637872a962b9200851
GabriRDiaz <correoclasegabri@gmail.com>
2021-03-11 13:56:19 +0100

    DONE

--- src/main/java/neodatis/Main.java
+++ src/main/java/neodatis/Main.java
@@ -2,6 +2,7 @@ package neodatis;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import pojo.Departamentos;
 import pojo.Empleados;
@@ -10,13 +11,55 @@ public class Main {
 	static Object[] info = new Object[2];
 	static ArrayList<Empleados> empleados = new ArrayList<Empleados>();
 	static ArrayList<Departamentos> departamentos = new ArrayList<Departamentos>();
-	public static void main(String[] args) throws IOException {
-		/*info = ReadCsv.read();
-		departamentos = (ArrayList<Departamentos>) info[0];
-		empleados = (ArrayList<Empleados>) info[1];
-		NeoDatisUtils.saveInfo(departamentos,empleados);
-	*/
-//		NeoDatisUtils.showInfo();
-		NeoDatisUtils.makeQueries();
+	public static void main(String[] args) throws IOException, InterruptedException {
+		showMenu();
+	}
+	private static void showMenu() throws IOException, InterruptedException {
+		System.out.println("*--------*");
+		System.out.println("|NEODATIS|");
+		System.out.println("*--------*");
+		System.out.println("1. Guardar CSV");
+		System.out.println("2. Visualizar información");
+		System.out.println("3. Ejecutar consultas");
+		System.out.println("4. Salir");
+		System.out.print("Seleccione una opción: ");
+		int option = readInfo();
+		switch (option) {
+		case 1:
+			info = ReadCsv.read();
+			departamentos = (ArrayList<Departamentos>) info[0];
+			empleados = (ArrayList<Empleados>) info[1];
+			NeoDatisUtils.saveInfo(departamentos,empleados);
+			showMenu();
+			break;
+		case 2:
+			NeoDatisUtils.showInfo();
+			showMenu();
+			break;
+		case 3:
+			NeoDatisUtils.makeQueries();
+			showMenu();
+			break;
+		case 4:
+			System.out.print("Terminating program");
+			for(int i=0;i<3;i++) {
+				Thread.sleep(600);
+				System.out.print(".");
+			}
+			System.out.println("\nProgram finished");
+			break;
+		}
+	}
+	private static int readInfo() throws IOException, InterruptedException {
+		Scanner scan = new Scanner(System.in);
+		int option = 0;
+		try {
+			 option = scan.nextInt();
+			if(option<1 || option >4) {
+				System.out.println("Valor inválido");
+				showMenu();
+				}
+		}catch(Exception ex) {System.out.println("Valor inválido"); showMenu();}
+		return option;
 	}
 }
--- src/main/java/neodatis/NeoDatisUtils.java
+++ src/main/java/neodatis/NeoDatisUtils.java
@@ -19,9 +19,10 @@ import pojo.Empleados;
 import org.neodatis.odb.Objects;
 import org.neodatis.odb.Values;
 public class NeoDatisUtils {
-	static ODB odb = ODBFactory.open("D:\\2DAM\\AAD\\Ejercicios\\NeoDatis\\db.neodatis");
+	static ODB odb;
 	static IQuery query;
 	public static void saveInfo(ArrayList...values) {
+		odb = ODBFactory.open("D:\\2DAM\\AAD\\Ejercicios\\NeoDatis\\db.neodatis");
 		Stream<ArrayList> stream = Stream.of (values);
 		stream.forEach(l->{
 			l.forEach(o->{
@@ -33,18 +34,21 @@ public class NeoDatisUtils {
 	}
 	
 	public static void showInfo() {
+		odb = ODBFactory.open("D:\\2DAM\\AAD\\Ejercicios\\NeoDatis\\db.neodatis");
 		query = new CriteriaQuery(Departamentos.class);
 		Objects<Departamentos> dep = odb.getObjects(query);
-		System.out.println("Departamentos");
-		System.out.println("---------------------------------------");
+		System.out.println("*-------------*");
+		System.out.println("|Departamentos|");
+		System.out.println("*-------------*");
+		System.out.println("*-------------------------------*");
 		for(Departamentos deps:dep) {
 			System.out.println("Código: "+deps.getCodigoD()+"\nNombre: "
-					+deps.getNombreD()+"\nLocalidad: "+deps.getLocalidad()+"\n*-------------------*");
+					+deps.getNombreD()+"\nLocalidad: "+deps.getLocalidad()+"\n*-------------------------------*");
 		}
-		System.out.println("---------------------------------------");
-		
-		System.out.println("Empleados");
-		System.out.println("---------------------------------------");
+		System.out.println("*---------*");
+		System.out.println("|Empleados|");
+		System.out.println("*---------*");
+		System.out.println("*-------------------------------*");
 		query = new CriteriaQuery(Empleados.class);
 		
 		Objects<Empleados> emp = odb.getObjects(query);
@@ -53,37 +57,64 @@ public class NeoDatisUtils {
 			query = new CriteriaQuery(Departamentos.class, Where.equal("codigoD", emps.getDepartamento()));
 			deptos = odb.getObjects(query);
 			System.out.println("Código: "+emps.getCodigoE()+"\nNombre: "
-					+emps.getNombreE()+"\nApellidos: "+emps.getApellidos()+"\nDepartamento: "+deptos.getFirst().getNombreD()+"\n*-------------------*");
+					+emps.getNombreE()+"\nApellidos: "+emps.getApellidos()+"\nDepartamento: "+deptos.getFirst().getNombreD()+"\n*-------------------------------*");
 		}
-		System.out.println("---------------------------------------");
+		odb.close();
 	}
 	
 	public static void makeQueries() {
+		odb = ODBFactory.open("D:\\2DAM\\AAD\\Ejercicios\\NeoDatis\\db.neodatis");
+		Objects<Empleados> emp = null;
+		Objects<Departamentos> dep = null;
+		CriteriaQuery cQuery = null;
+		ObjectValues ov = null;
+		Values valueGroup = null;
+		//Nombre empleados dep 100
+		System.out.println("*-------------------------------*");
+		System.out.println("|Empleados del departamento 100 |");
+		System.out.println("*-------------------------------*");
+		query1(emp);
+		//Número empleados dep QA
+		System.out.println("*------------------------------------*");
+		System.out.println("|Número empleados del departamento QA|");
+		System.out.println("*------------------------------------*");
+		query2(cQuery, valueGroup, ov);
+		//Empleados por depto
+		System.out.println("*---------------------------------*");
+		System.out.println("|Número empleados por departamento|");
+		System.out.println("*---------------------------------*");
+		query3(dep,cQuery, emp,valueGroup,ov);
+		System.out.println("*---------------------------------*");
+		
+		odb.close();
+	}
+
+	private static void query3(Objects<Departamentos> dep, CriteriaQuery cQuery, Objects<Empleados> emp, Values valueGroup, ObjectValues ov) {
+		cQuery = new CriteriaQuery(Empleados.class);
+		emp = odb.getObjects(cQuery);
+		dep = odb.getObjects(new CriteriaQuery(Departamentos.class));
+		valueGroup = odb.getValues(new
+			ValuesCriteriaQuery(Empleados.class).count("nombreE").groupBy("departamento"));	
+		for(Departamentos deps:dep) {
+				ov=valueGroup.next();
+				System.out.println("Departamento "+deps.getNombreD()+": " +ov.getByIndex(0));
+			}
+	}
+
+	private static void query2(CriteriaQuery cQuery, Values valueGroup, ObjectValues ov) {
+		cQuery = new CriteriaQuery(Empleados.class, Where.equal("departamento",300));
+		valueGroup = odb.getValues(new ValuesCriteriaQuery(cQuery).count("nombreE"));
+		ov = valueGroup.nextValues();
+		BigInteger num = (BigInteger)ov.getByAlias("nombreE");
+		System.out.println("Número empleados QA: "+num);
+	}
+
+	private static void query1(Objects<Empleados> emp) {
 		//Nombre empleados dep 100
 		query = new CriteriaQuery(Empleados.class, Where.equal("departamento", 100));
-		Objects<Empleados> emp = odb.getObjects(query);
+		emp = odb.getObjects(query);
 		for(Empleados emps:emp) {
 			System.out.println("Nombre del empleado: "+emps.getNombreE());
 		}
-		//Número empleados dep QA
-		CriteriaQuery cQuery = new CriteriaQuery(Empleados.class, Where.equal("departamento",300));
-		Values value = odb.getValues(new ValuesCriteriaQuery(cQuery).count("nombreE"));
-		ObjectValues ov= value.nextValues();
-		BigInteger num = (BigInteger)ov.getByAlias("nombreE");
-		System.out.println("Número empleados QA: "+num);
-	
-		//Empleados por depto
-		
-		cQuery = new CriteriaQuery(Empleados.class);
-		CriteriaQuery cQueryEmp;
-		emp = odb.getObjects(cQuery);
-		Objects<Departamentos> dep = odb.getObjects(new CriteriaQuery(Departamentos.class));
-		Values valores = odb.getValues(new
-			ValuesCriteriaQuery(Empleados.class).count("nombreE").groupBy("departamento"));	
-		for(Departamentos deps:dep) {
-				ov=valores.next();
-				System.out.println("Departamento "+deps.getNombreD()+": " +ov.getByIndex(0));
-			}
-	
 	}
 }
